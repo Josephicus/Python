93ba7725d628bcabdf69b26e540d8da9aa76861c
Josephicus <pawel.zaw@gmail.com>
2016-05-23 00:14:51 +0200

    update4

--- name-input.py
+++ name-input.py
@@ -1,16 +1,16 @@
 
 
 
-name = input("Podaj swoje imię ")
+name = input('Podaj swoje imię ')
 lname=name.lower()
 
 
-if lname==("maria"):
+if lname==('maria'):
     print(lname)
-    print("Cfane, ale możesz by i kobietą i mężczyzną")
-elif lname==(""):
-    print("Musisz podac jakąs wartosc")
-elif lname.endswith("a"):
+    print('Cfane, ale możesz by i kobietą i mężczyzną')
+elif lname==(''):
+    print('Musisz podac jakąs wartosc')
+elif lname.endswith('a'):
     print(lname)
     print('jestes kobieta')
 else:
