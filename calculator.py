b176af31a6f62a5090875aa8625cc4c5594f22cf
Josephicus <pawel.zaw@gmail.com>
2016-07-27 23:53:27 +0200

    calc scrypt

---
+++ calculator.py
@@ -0,0 +1,39 @@
+#calculator
+#informacje startowe
+
+print('Calculator')
+
+#wyświetlenie opcji
+print('Dodawanie (1)')
+print('Odejmowanie (2)')
+print('Mnożenie (3)')
+print('Dzielenie (4)')
+print('Potęgowanie (5)')
+print('Pierwiastkowanie (6)')
+
+#wybór działania
+choice = input('Wybierz co chcesz zrobić:')
+print(choice)
+
+# Liczby
+liczba1 = input('Podaj pierwszą liczbę: ')
+liczba2 = input('Podaj drugą liczbę: ')
+
+# obliczenia
+	if choice ==  ('1'):
+		Dodawanie()
+
+
+
+def Dodawanie():
+	print('Wybrałeś dodawanie')
+
+	wynik = liczba1 + liczba2
+
+
+#wyświetlnie wyniku
+def Wynik():
+	pass
+
+
+blank = input()
